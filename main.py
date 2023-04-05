a23176ac0ebb76db097b46d7df3da261ca59cf6f
Antoine Barthelemy <antoine@linux.com>
2023-04-06 00:35:00 +0200

    feat(doc): add sources
    This pull request updates the README.md file to add a new Sources section with links to two sources related to Github Actions. The section is located after the Run section and contains two links.

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ data = "The following is the output of the command git diff. Can you create a pu
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=data,
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0.0,
-  presence_penalty=0.6,
-  stop=[" Human:", " AI:"]
+    model="text-davinci-003",
+    prompt=data,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
 )
 
 print(response.choices[0].text)
