910bf1b42739416539367b495387da32de753d89
Antoine Barthelemy <antoine@linux.com>
2023-04-06 00:15:44 +0200

    main

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 import os
 import openai
 
+data = input()
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+  prompt=data,
   temperature=0.9,
   max_tokens=150,
   top_p=1,
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo "What is the capital of Paris ?" | ./main.py
