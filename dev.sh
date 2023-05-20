ac84f45058d61be8fd3571eb777faeb6fcb40a27
Antoine Barthelemy <antoine@linux.com>
2023-05-20 11:40:44 +0200

    hop

--- dev.sh
+++ dev.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
-git diff HEAD^ HEAD | python src/main.py
+set -o errexit
 
-#gh pr diff <url> --patch | ./main.py
+FILE='short_commit_sha'
+CONTENT="$(git rev-parse --short HEAD)"
+ID="$(tr -dc '[:alpha:]' < /dev/urandom | fold -w 1 | head -n 1)$(tr -dc '[:digit:]' < /dev/urandom | fold -w 3 | head -n 1)"
+BRANCH="testPR-$ID"
+
+git checkout -b "$BRANCH"
+echo "$CONTENT" > "$FILE"
+
+git add "$FILE"
+git commit -m "init(pr): add file for test"
+git push --set-upstream origin "$BRANCH"
+
+gh pr create --title "[TEST] - PR $ID" --body ""
+
+python src/main.py
+gh pr view
+
+git checkout main
+gh pr close "$BRANCH" --delete-branch
