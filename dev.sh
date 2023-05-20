03821b21aabb2c5a1a2d202ac34b304f0859487e
Antoine Barthelemy <antoine@linux.com>
2023-05-20 10:40:35 +0200

    feat(script): test pr first commit

--- dev.sh
+++ dev.sh
@@ -4,3 +4,7 @@ git diff HEAD^ HEAD | python src/main.py
 #gh pr diff <url> --patch | ./main.py
 git checkout -b test-pr
 echo "echo \"OK\"" > script.sh
+git add script.sh
+git commit -m "feat(script): test pr first commit"
+git push
+
