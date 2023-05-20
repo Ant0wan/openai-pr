7d419a85a1aea5db910ee24499e120e7cc1696a8
Antoine Barthelemy <antoine@linux.com>
2023-05-20 18:41:03 +0200

    update

--- src/configuration/parse.py
+++ src/configuration/parse.py
@@ -1,13 +1,56 @@
+"""
+This module provides a YAML file parser class.
+
+The module includes a class, 'Yaml', that can be used to load and parse a YAML file. The YAML file is loaded
+using the PyYAML library and the 'SafeLoader' loader. The parsed YAML data can be accessed through the 'conf'
+property of the 'Yaml' class.
+
+Example usage:
+--------------
+yaml_file = "config.yaml"
+yaml_parser = Yaml(yaml_file)
+data = yaml_parser.conf
+
+"""
 import yaml
 from yaml.loader import SafeLoader
 
 
 class Yaml:
+    """
+    A YAML file parser class.
+
+    The 'Yaml' class can be used to load and parse a YAML file. The parsed YAML data can be accessed through
+    the 'conf' property.
+
+    Example usage:
+    --------------
+    yaml_file = "config.yaml"
+    yaml_parser = Yaml(yaml_file)
+    data = yaml_parser.conf
+    """
 
     def __init__(self, yamlfile: str):
+        """
+        Initialize the YAML parser with the provided YAML file.
+
+        The YAML file is loaded using the PyYAML library and the 'SafeLoader' loader.
+
+        Args:
+            yamlfile (str): The path to the YAML file.
+
+        Returns:
+            None
+        """
         with open(yamlfile) as f:
             self.__conf = yaml.load(f, Loader=SafeLoader)
 
     @property
     def conf(self) -> dict:
+        """
+        Get the parsed YAML data as a dictionary.
+
+        Returns:
+            dict: The parsed YAML data.
+        """
         return self.__conf
