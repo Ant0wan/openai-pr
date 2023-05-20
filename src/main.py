e37699b14f7641d7d32940aa0ec460c31209b91e
Antoine Barthelemy <antoine@linux.com>
2023-05-20 23:52:10 +0200

    test

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ def main():
     """
     actionpath = os.environ.get('GITHUB_ACTION_PATH')
     if actionpath:
-        yamlfile = f"{path}/src/config.yaml"
+        yamlfile = f"{actionpath}/src/config.yaml"
     else:
         yamlfile = 'config.yaml'
     config = parse.Yaml(yamlfile).conf
