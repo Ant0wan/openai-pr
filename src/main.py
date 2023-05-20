41f06cd84e46639c5ea8dd14f6881bd22f56e7d4
Antoine Barthelemy <antoine@linux.com>
2023-05-20 18:37:43 +0200

    test

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ def main():
     logs.init(config)
     env = preflight.Env(config)
 
-    github_token = env.vars['GH_TOKEN']
+    github_token = env.vars['GITHUB_TOKEN']
     pullrequest = pr.PullRequest(github_token)
     logging.info(pullrequest)
 
