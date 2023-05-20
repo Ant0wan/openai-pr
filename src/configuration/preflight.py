9d74d247f8ef00a93cf80c76a0f2cecff2b8b37b
Antoine Barthelemy <antoine@linux.com>
2023-05-20 18:42:44 +0200

    update

--- src/configuration/preflight.py
+++ src/configuration/preflight.py
@@ -3,15 +3,55 @@ import logging
 
 
 class Env:
+    """
+    An environment variable loader class.
+
+    The 'Env' class can be used to load and check environment variables based on a provided configuration.
+    The loaded environment variables can be accessed through the 'vars' property.
+
+    Example usage:
+    --------------
+    config = {
+        'preflights': {
+            'env': ['ENV_VAR_1', 'ENV_VAR_2']
+        }
+    }
+
+    env_loader = Env(config)
+    variables = env_loader.vars
+    """
 
     def __init__(self, config: dict):
+        """
+        Initialize the environment variable loader with the provided configuration.
+
+        The configuration dictionary should contain a 'preflights' key, which itself should contain an 'env'
+        key specifying the required environment variables. The 'Env' class performs preflight checks to ensure
+        that all the specified environment variables are present.
+
+        Args:
+            config (dict): A dictionary containing the configuration parameters.
+
+        Returns:
+            None
+        """
         self.__vars = self._preflight_checks(
             config['preflights']['env'])
         logging.debug(self.__vars)
 
     @staticmethod
     def _preflight_checks(var_names: list) -> dict:
-        """Check all needed environment variables
+        """
+        Perform preflight checks to ensure that all the specified environment variables are present.
+
+        Args:
+            var_names (list): A list of environment variable names.
+
+        Returns:
+            dict: A dictionary containing the loaded environment variables.
+
+        Raises:
+            KeyError: If any of the required environment variables is missing.
         """
         env_variables = {}
         for var in var_names:
@@ -24,4 +64,10 @@ class Env:
 
     @property
     def vars(self):
+        """
+        Get the loaded environment variables as a dictionary.
+
+        Returns:
+            dict: The loaded environment variables.
+        """
         return self.__vars
