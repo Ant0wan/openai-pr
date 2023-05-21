17d6aa06023782101591ce1ff426652fc43a8fff
Antoine Barthelemy <antoine@linux.com>
2023-05-21 12:27:59 +0200

    test

---
+++ tag.sh
@@ -0,0 +1,8 @@
+set +x
+git add --all
+git commit -m "test"
+git push
+TAG="$(git tag | sort -V | tail -n1)"
+NEWTAG="0.0$(echo "$(git tag | sort -V | tail -n1 | cut -d '.' -f2,3) + 0.01" | bc)"
+git tag $NEWTAG
+git push --tag
