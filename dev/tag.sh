07f95529513ba8a968256aac2ab7c209de9e1eba
Antoine Barthelemy <antoine@linux.com>
2023-05-21 14:12:07 +0200

    update

--- dev/tag.sh
+++ dev/tag.sh
@@ -1,9 +1,9 @@
+#!/bin/sh
 set +x
 git add --all
 git commit -m "test"
 git push
-TAG="$(git tag | sort -V | tail -n1)"
 NEWTAG="0.0$(echo "$(git tag | sort -V | tail -n1 | cut -d '.' -f2,3) + 0.01" | bc)"
-git tag $NEWTAG
+git tag "$NEWTAG"
 git push --tag
-gh release create $NEWTAG --title "$NEWTAG" --notes ""
+gh release create "$NEWTAG" --title "$NEWTAG" --notes ""
