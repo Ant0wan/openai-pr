9f9a06269d2cf5cc7952d1fb472fbf65b6b4a24c
Antoine Barthelemy <antoine@linux.com>
2023-05-29 17:27:39 +0200

    test main

--- src/tests/test_preflight.py
+++ src/tests/test_preflight.py
@@ -1,10 +1,17 @@
+"""
+Unit tests for the Env class in the configuration.preflight module.
+"""
+
 import unittest
 from unittest.mock import patch
 from configuration.preflight import Env
 
 
 class TestEnv(unittest.TestCase):
+    """Test case for the Env class."""
+
     def test_init(self):
+        """Test the initialization of Env class."""
         config = {
             'preflights': {
                 'env': ['ENV_VAR_1', 'ENV_VAR_2']
@@ -18,6 +25,7 @@ class TestEnv(unittest.TestCase):
         self.assertEqual(env_loader.vars, env_vars)
 
     def test_str(self):
+        """Test the string representation of Env class."""
         env_vars = {'ENV_VAR_1': 'value1', 'ENV_VAR_2': 'value2'}
         env_loader = Env({
             'preflights': {
@@ -30,6 +38,7 @@ class TestEnv(unittest.TestCase):
         self.assertEqual(str(env_loader), expected_output)
 
     def test_vars(self):
+        """Test the vars property of Env class."""
         env_vars = {'ENV_VAR_1': 'value1', 'ENV_VAR_2': 'value2'}
         env_loader = Env({
             'preflights': {
